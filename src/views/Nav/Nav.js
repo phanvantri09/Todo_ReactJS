a1b5c0e49f61db4ef1bd0422199219698cb55acb
phongnv9 <phongnv9@rikkeisoft.com>
2022-11-28 16:41:44 +0700

    use navlink to menu bar

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Nav.scss'
+import {
+    NavLink,
+    Link
+  } from "react-router-dom";
 class Nav extends React.Component {
     render () {
         return(
+            // should use navlink, because navlink support an function as: active class, exact sẻ chỉ load page / khi đúng thôi tránh việc trùng với namepage khác
             <div class="topnav">
-                <a class="active" href="/">Home</a>
-                <a href="/users">User - todo</a>
-                <a href="/todo">Todo</a>
+                <NavLink to="/" activeClassName="active" exact={true}>Home</NavLink>
+                <NavLink to="/users" activeClassName="active" >Users</NavLink>
+                <NavLink to="/todo" activeClassName="active" >todo</NavLink>
+
             </div>
         )
     }
